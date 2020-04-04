ba4f1d3543906ccdf7043ff7b93e158d00aaebef
Don <hello@donbos.co>
2020-04-04 16:00:30 +0800

    initial minor changes from the flask2html branch

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, This is a message coming from the Pi!'
+    return 'Hello, This is a message coming from the Pi! This message specifically is coming from my deeboss/flask2html branch'
