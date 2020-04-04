0f08157c7a2b734e1ba99d22927fb3d9380ef813
Don <hello@donbos.co>
2020-04-04 16:53:12 +0800

    freezing python view and css for now

--- app.py
+++ app.py
@@ -2,11 +2,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, This is a message coming from the Pi! This message specifically is coming from my deeboss/flask2html branch'
-
-@app.route('/remote')
-def remote():
+def home():
     return render_template("index.html")
     
 if __name__ == "__main__":
