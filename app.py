2cff4cf146fb5dd34157c629e6af9bb1a0eadb65
Don <hello@donbos.co>
2020-04-04 16:30:22 +0800

    setting up initial render_templates

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template      
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, This is a message coming from the Pi! This message specifically is coming from my deeboss/flask2html branch'
+
+@app.route('/remote')
+def remote():
+    return render_template("index.html")
+    
+if __name__ == "__main__":
+    app.run(debug=True)
