d2450a5068a8e767d7f230c62f314dee69137028
Nick <nickmok@gmail.com>
2020-04-05 02:46:19 +0800

    changed it so green on-board led lights up when hotspot is on.

--- hotspot.sh
+++ hotspot.sh
@@ -2,12 +2,16 @@
 
 FILE=/etc/wpa_supplicant
 
+
 if test -f "$FILE/wpa_supplicant.conf"; then
    sudo mv $FILE/wpa_supplicant.conf $FILE/wpa_supplicant.conf.orig
    sudo /usr/bin/autohotspotN
+   echo gpio | sudo tee /sys/class/leds/led0/trigger
+   echo 1 | sudo tee /sys/class/leds/led0/brightness
 elif test -f "$FILE/wpa_supplicant.conf.orig"; then
    sudo mv $FILE/wpa_supplicant.conf.orig $FILE/wpa_supplicant.conf
    sudo /usr/bin/autohotspotN
+   echo input | sudo tee /sys/class/leds/led0/trigger
 else
    echo "Neither .conf or .conf.orig file could be found!"
 fi
