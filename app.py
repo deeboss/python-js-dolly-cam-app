bf4740ba7dbbd665e27c5dd9e887e6d6a6a7c777
Don <hello@donbos.co>
2020-04-04 17:48:23 +0800

    merging blink led with client side

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+import RPi.GPIO as GPIO    # Import Raspberry Pi GPIO library
+import os
+import sys
+import time     # Import the sleep function from the time module
+
 from flask import Flask, jsonify, render_template, request
 app = Flask(__name__)
 
@@ -11,7 +16,18 @@ def showAlert():
     a = request.args.get('a', 0, type=int)
     b = request.args.get('b', 0, type=int)
     return jsonify(result=a + b)
-    
+
+@app.route('/blinkLed')
+def blinkLed():
+    os.system("sudo echo gpio | sudo tee /sys/class/leds/led0/trigger")
+    for n in range(0, 5):
+        os.system("echo 1 | sudo tee /sys/class/leds/led1/brightness")
+        time.sleep(1)
+        os.system("echo 0 | sudo tee /sys/class/leds/led1/brightness")
+        time.sleep(1)
+
+    return
+
 
 
 if __name__ == "__main__":
