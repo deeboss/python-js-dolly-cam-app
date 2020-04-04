7b9c1a9656372da5a93745a49dfa411e829ff2d1
Don <hello@donbos.co>
2020-04-04 17:03:58 +0800

    adding jquery for ajax support

--- app.py
+++ app.py
@@ -5,5 +5,11 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
     
+
+# @app.route('/showAlert')
+# def alert():
+    
+
+
 if __name__ == "__main__":
     app.run(debug=True)
