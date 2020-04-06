169295ec92b759f62db98cce5de571812bd8e6eb
Nick <nickmok@gmail.com>
2020-04-06 15:18:54 +0800

    this script intializes hotspot, venv, and server. Need to remember to switch off hotspot after exiting this script.

---
+++ initialize.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+#Turn on hotspot
+os.system("/home/pi/Documents/python-dolly-cam-app/hotspot.sh")
+
+#Turn on venv and run server
+os.system("/home/pi/Documents/python-dolly-cam-app/app.py")
