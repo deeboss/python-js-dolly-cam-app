27102177ff1e5e92822b093df6c1c91540f2f4ab
Nick <nickmok@gmail.com>
2020-04-08 01:49:15 +0800

    added venv detection, still under works.

--- initialize.py
+++ initialize.py
@@ -1,8 +1,15 @@
 import os
 import sys
 
-#Turn on hotspot
+'''
+# Create venv and install packages if they do not exist
+if not os.path.exists("/home/pi/venv"):
+    os.system("python3 -m venv venv)
+    os.system("pip install -r requirements.rpi.text")
+'''
+
+# Turn on hotspot
 os.system("/home/pi/Documents/python-dolly-cam-app/hotspot.sh")
 
-#Turn on venv and run server
+# Turn on venv and run server
 os.system("/home/pi/Documents/python-dolly-cam-app/app.py")
