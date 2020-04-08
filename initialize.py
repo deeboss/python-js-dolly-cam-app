1852179957bd9d0388ec1180bd92ee4dced9e7ac
Nick <nickmok@gmail.com>
2020-04-08 22:12:31 +0800

    added in button activation. still lacking button deactivation.

--- initialize.py
+++ initialize.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import RPi.GPIO as GPIO
+import time
 
 '''
 # Create venv and install packages if they do not exist
@@ -8,8 +10,14 @@ if not os.path.exists("/home/pi/venv"):
     os.system("pip install -r requirements.rpi.text")
 '''
 
-# Turn on hotspot
-os.system("/home/pi/Documents/python-dolly-cam-app/hotspot.sh")
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-# Turn on venv and run server
-os.system("/home/pi/Documents/python-dolly-cam-app/app.py")
+while True:
+   if GPIO.input(18) == True:
+      # Turn on hotspot
+      os.system("/home/pi/Documents/python-dolly-cam-app/hotspot.sh")
+
+      # Turn on venv and run server
+      os.system("/home/pi/Documents/python-dolly-cam-app/app.py")
+   time.sleep(0.1)
