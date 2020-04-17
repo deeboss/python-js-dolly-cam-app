0fc641a705aaf37ceb0b6edc871c0ce3c7b62733
Don <hello@donbos.co>
2020-04-17 18:35:00 +0800

    adding the missing routes

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,4 +2,6 @@ from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 app_template_views = Blueprint('app_template_views', __name__, template_folder='templates')
 from views.index import *  # noqa
+from views.gpio import *  # noqa
+from views.stepper import *  # noqa
 from views.camera import *  # noqa
--- api/v1/views/camera.py
+++ api/v1/views/camera.py
@@ -1,6 +1,7 @@
 """
 Flask route that returns json status response
 """
+import os
 from views import app_views
 from flask import Flask, jsonify, json, render_template, request
 # from models import filename_of_model
@@ -12,3 +13,17 @@ def test():
     """
     data = {"status": "OK"}
     return jsonify(data)
+
+@app_views.route('/captureImage', methods=['GET', 'POST'])
+def captureImage():
+    os.system("gphoto2 --capture-image")
+    return jsonify("Works")
+
+@app_views.route('/changeCameraSettings', methods=['GET', 'POST'])
+def changeSettings():
+    optionName = request.args.get('optionName', 0, type=str)
+    optionValue = request.args.get('optionValue', 0, type=str)
+
+    os.system('gphoto2 --set-config={}={}'.format(optionName, optionValue))
+
+    return jsonify("OK!")
