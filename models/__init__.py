ccdd8d8a99348b2fb322f1602296c28dc7ab6bb0
Don <hello@donbos.co>
2020-04-17 16:43:19 +0800

    adding models and api restructruing

---
+++ models/__init__.py
@@ -0,0 +1,2 @@
+import os
+# from models.stepper import Stepper
---
+++ models/stepper.py
@@ -0,0 +1,14 @@
+"""
+Stepper Class from Models Module
+"""
+import os
+
+class Stepper:
+    """
+        Stepper class handles all application users
+    """
+
+    def __init__(self):
+        """
+            instantiates stepper object
+        """
