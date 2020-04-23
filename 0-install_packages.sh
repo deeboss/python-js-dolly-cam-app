292733a9f88ea0b26cab5a0d3f597f92c4f532a2
Don <hello@donbos.co>
2020-04-23 15:45:04 +0800

    fixing gpio package name

--- 0-install_packages.sh
+++ 0-install_packages.sh
@@ -4,7 +4,7 @@
 
 cd ../../
 apt-get update  # To get the latest package lists
-apt-get install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev python-rpi.gpio python3-rpi.gpio gphoto2 -y
+apt-get install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev rpi.gpio gphoto2 -y
 wget https://www.python.org/ftp/python/3.7.1/Python-3.7.1.tgz
 cd Python-3.7.1
 ./configure
