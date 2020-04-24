a79f9a8158cc3f86c39da873a7d07c78e5f31eec
Nick <nickmok@gmail.com>
2020-04-24 17:09:07 +0800

    sandbox test with pwm. stepper file added waypoint buttons.

--- api/v1/views/stepper.py
+++ api/v1/views/stepper.py
@@ -20,6 +20,9 @@ class motorClass:
     def __init__(self):
         self.motorMove = False
         self.stepsTaken = 0
+        self.waypointOneSteps = 0
+        self.waypointTwoSteps = 0
+        self.waypointThreeSteps = 0
     
     # Motor move
     def Move(self):  
@@ -28,15 +31,33 @@ class motorClass:
             GPIO.output(13,False)
             time.sleep(0.001)
             
+            # counting steps
             if GPIO.input(11) == True:
                 self.stepsTaken+=1
             elif GPIO.input(11) == False:
                 self.stepsTaken-=1
-
+                
+    # Manual input forward and backward buttons
     def Start(self):
         self.motorMove = True
     def Stop(self):
         self.motorMove = False
+        
+    # Moving to waypoint
+    def gotoWaypoint(self,waypointSteps):
+        if (self.stepsTaken - waypointSteps) > 0:
+            sign=-1
+        elif (self.stepsTaken - waypointSteps) < 0:
+            sign=+1
+            
+        for i in range(self.stepsTaken,waypointSteps,sign):
+            GPIO.output(13,True)
+            GPIO.output(13,False)
+            time.sleep(0.001)
+            
+        self.stepsTaken=waypointSteps
+        
+    
         
 # Run class after initializing
 motorClass = motorClass()
@@ -44,6 +65,7 @@ motorClass = motorClass()
 
 ## BUTTONS ##
 
+# Manual movement buttons
 @app_views.route('/forwardStart')
 def forwardStart():
     GPIO.output(11,True) # set direction
@@ -79,10 +101,7 @@ def rewind():
         direction = True
     else:
         return jsonify("OK")
-    
-    print(motorClass.stepsTaken)
-    print(sign)
-    
+
     for i in range(0,motorClass.stepsTaken,sign):
         GPIO.output(11,direction)
         GPIO.output(13,True)
@@ -90,5 +109,39 @@ def rewind():
         time.sleep(0.001)
 
     motorClass.stepsTaken = 0
+    return jsonify("OK")
+
+
+# Save waypoint buttons
+@app_views.route('/saveWaypointOne')
+def saveWaypointOne():
+    motorClass.waypointOneSteps=motorClass.stepsTaken
+    return jsonify("OK")
+
+@app_views.route('/saveWaypointTwo')
+def saveWaypointTwo():
+    motorClass.waypointTwoSteps=motorClass.stepsTaken
+    return jsonify("OK")
+
+@app_views.route('/saveWaypointThree')
+def saveWaypointThree():
+    motorClass.waypointThreeSteps=motorClass.stepsTaken
+    return jsonify("OK")
+
+
+# Move to waypoint buttons
+@app_views.route('/runWaypointOne')
+def runWaypointOne():
+    motorClass.gotoWaypoint(motorClass.waypointOneSteps)
+    return jsonify("OK")
+
+@app_views.route('/runWaypointTwo')
+def runWaypointTwo():
+    motorClass.gotoWaypoint(motorClass.waypointTwoSteps)
+    return jsonify("OK")
+
+@app_views.route('/runWaypointThree')
+def runWaypointThree():
+    motorClass.gotoWaypoint(motorClass.waypointThreeSteps)
     return jsonify("OK")
     
