ef1312f6431c00a5243902a64a8e257362be749b
Nick <nickmok@gmail.com>
2020-04-24 17:10:18 +0800

    new testing file for pwm.

--- sandbox/big_stepper.py
+++ sandbox/big_stepper.py
@@ -5,38 +5,12 @@ GPIO.setmode(GPIO.BOARD)
 
 # Initialize pins
 GPIO.setup(11,GPIO.OUT) # direction
-GPIO.setup(13,GPIO.OUT) # step
-#GPIO.setup(15,GPIO.OUT) # sleep
-#GPIO.setup(16,GPIO.OUT) # reset
+GPIO.setup(12,GPIO.OUT)
+p = GPIO.PWM(12,1000)
+time=0
+while True:
+    p.start(25)
+    
 
-
-# First reset
-#GPIO.output(15,GPIO.LOW)
-
-# Sleep
-#GPIO.output(15,GPIO.HIGH)
-'''
-# Pseudo step (for some reason this makes the motor run smoothly after a fresh start even though motor doesn't actually rotate)
-for i in range(0,200):
-    GPIO.output(13,True)
-    GPIO.output(13,False)
-    time.sleep(0.001)
-'''
-# Wake up
-#GPIO.setup(15,GPIO.OUT)
-#GPIO.output(15,GPIO.HIGH)
-
-# Second reset
-#GPIO.output(15,GPIO.HIGH)
-
-
-# Step forward and back
-for y in range(0,3):
-    for x in range(0,2):
-        GPIO.output(11,x)
-        for i in range(0,200):
-            GPIO.output(13,True)
-            GPIO.output(13,False)
-            time.sleep(0.001)
 
 GPIO.cleanup()
