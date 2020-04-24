f3567a767c00fc097bea04177b62e5c99b686192
deeboss <hello@donbos.co>
2020-04-24 20:24:30 +0100

    sudo permission and order of operations adjustments

--- 0-first_time_setup.sh
+++ 0-first_time_setup.sh
@@ -7,4 +7,11 @@ wget https://raw.githubusercontent.com/km4ack/pi-scripts/master/autohotspotN-set
 sudo chmod +x autohotspotN-setup
 sudo ./autohotspotN-setup
 
-sed -i -e "s/${ORIGINAL}/${NEW}/g" /etc/hostapd/hostapd.conf
+sudo sed -i -e "s/${ORIGINAL}/${NEW}/g" /etc/hostapd/hostapd.conf
+
+. ${SOURCE}/2-updater.sh
+
+zenity --info --title 'System Reboot Required' --text "The device needs to reboot to save the configurations. Automatic reboot in 5 seconds." --width=500 --height=320 &
+sleep 5
+kill $zpid
+sudo reboot
