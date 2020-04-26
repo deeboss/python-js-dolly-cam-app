a5e704d6e03a1626684aeb1fc67f59889fee7e0d
Don <hello@donbos.co>
2020-04-26 22:24:21 +0800

    reordering removals of files

--- 1-updater.sh
+++ 1-updater.sh
@@ -6,17 +6,19 @@ TARGET=/home/pi/Downloads
 APP_NAME=python-dolly-cam-app
 
 # Ensure we're downloading to the Downloads folder
-cd /home/pi/Downloads/
+cd /home/pi/Documents/
 
 wget https://github.com/deeboss/python-dolly-cam-app/archive/${VERSION}.tar.gz
 
-tar -xzf ${VERSION}.tar.gz -C ${TARGET}
+tar -xzf ${VERSION}.tar.gz -C /home/pi/Documents
 # Delete the tar zip file to declutter Downloads folder
 rm -rf ${VERSION}.tar.gz
 
 # Renames the app, stripping away the version name and overwriting the existing app.
 # Thereby ensuring only the most updated app remains in the TARGET folder
-mv -u /home/pi/Downloads/python-dolly-cam-app-${VERSION} /home/pi/Downloads/python-dolly-cam-app
+rm -rf /home/pi/Downloads/python-dolly-cam-app
+
+mv -u /home/pi/Documents/python-dolly-cam-app-${VERSION} /home/pi/Downloads/python-dolly-cam-app
 cd /home/pi/Downloads/python-dolly-cam-app
 
 # Creates and activates virtual environment
