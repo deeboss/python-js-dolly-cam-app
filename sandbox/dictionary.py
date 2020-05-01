da6adb8edfc5e59342137315f96801a5fe33f286
deeboss <hello@donbos.co>
2020-05-02 00:38:26 +0800

    testing out dictionaries for easings

---
+++ sandbox/dictionary.py
@@ -0,0 +1,22 @@
+# Linear
+easingDict ={'Linear':[1,'In']}
+
+# Quadratic
+easingDict['QuadraticIn']=[2,'In']
+easingDict['QuadraticOut']=[2,'Out']
+easingDict['QuadraticInOut']=[2,'InOut']
+
+# Cubic
+easingDict['CubicIn']=[3,'In']
+easingDict['CubicOut']=[3,'Out']
+easingDict['CubicInOut']=[3,'InOut']
+
+# Quartic
+easingDict['QuarticIn']=[4,'In']
+easingDict['QuarticOut']=[4,'Out']
+easingDict['QuarticInOut']=[4,'InOut']
+
+# Quintic
+easingDict['QuinticOut']=[5,'Out']
+easingDict['QuinticInOut']=[5,'InOut']
+easingDict['QuinticIn']=[5,'In']
