396687c5c71dd952d3b06205718d80701b0072cd
deeboss <hello@donbos.co>
2020-05-02 20:05:45 +0800

    readding motor waypoint chaining

--- api/v1/views/motor.py
+++ api/v1/views/motor.py
@@ -141,23 +141,23 @@ def runSingleRoute():
     .format(routeFrom, routeTo, routeEasing, routeDuration))
     print("==================================================")
 
-    # lambdaRouteFrom = None
-    # lambdaRouteTo = None
+    lambdaRouteFrom = None
+    lambdaRouteTo = None
 
-    # try:
-    #     lambdaRouteFrom = getattr(motor, routeFrom)
-    #     lambdaRouteTo = getattr(motor, routeTo)
+    try:
+        lambdaRouteFrom = getattr(motor, routeFrom)
+        lambdaRouteTo = getattr(motor, routeTo)
 
-    # except AttributeError:
-    #     raise NotImplementedError("Class `{}` does not implement `{}`".format(motor.__class__.__name__, routeTo))
+    except AttributeError:
+        raise NotImplementedError("Class `{}` does not implement `{}`".format(motor.__class__.__name__, routeTo))
 
-    # # Compute time array for steps
-    # arr=easeFunctions.easeInOut(lambdaRouteTo-lambdaRouteFrom,routeDuration,routeEasing)
-    # #timeArray=easeFunctions(lambdaRouteFrom,lambdaRouteTo,routeDuration,routeEasing)
-    # print(abs(lambdaRouteTo-lambdaRouteFrom))
-    # print(len(arr))
+    # Compute time array for steps
+    arr=easeFunctions.easeInOut(lambdaRouteTo-lambdaRouteFrom,routeDuration,routeEasing)
+    #timeArray=easeFunctions(lambdaRouteFrom,lambdaRouteTo,routeDuration,routeEasing)
+    print(abs(lambdaRouteTo-lambdaRouteFrom))
+    print(len(arr))
 
-    # # Run time array
-    # easeFunctions.runEaseFunctions(lambdaRouteTo-lambdaRouteFrom,arr)
-    # motor.stepsTaken = lambdaRouteTo
+    # Run time array
+    easeFunctions.runEaseFunctions(lambdaRouteTo-lambdaRouteFrom,arr)
+    motor.stepsTaken = lambdaRouteTo
     return jsonify(200)
