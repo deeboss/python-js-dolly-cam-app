af85fc77e51cda0f536872b9e21f6ac1dde3286c
Don <hello@donbos.co>
2020-06-24 20:52:46 +0800

    keypress down and up event listeners

--- client/src/components/KeyboardControlsCanvas.js
+++ client/src/components/KeyboardControlsCanvas.js
@@ -3,75 +3,49 @@ import '../assets/css/styles.scss';
 import { AppSettingsContext } from '../contexts/AppSettingsContext';
 
 const KeyboardControlsCanvas = () => {
-    const { status }  = useContext(AppSettingsContext);
-
-
-    function handleKeyDown(e) {
-
-        switch(e.key) {
-            case "ArrowUp":
-                console.log("ArrowUp!");
-                break;
-            case "ArrowDown":
-                console.log("ArrowDown!");
-                break;
-            case "ArrowLeft":
-                console.log("ArrowLeft!");
-                break;
-            case "ArrowRight":
-                console.log("ArrowRight!");
-                break;
-
-            case "Escape":
-                console.log("Escape!");
-                break;
-
-            case "z":
-                console.log("z");
-                break;
-
-            case "x":
-                console.log("x");
-                break;
-
-            case "c":
-                console.log("c");
-                break;
-
-            case "r":
-                console.log("r");
-                break;
-                
-            default:
-                break;
+    const { activeKeystroke, setActiveKeystroke, handleKeyDown, handleKeyUp }  = useContext(AppSettingsContext);
+    
+    const highlightActiveKey = (target) => {
+        const highlightedKey = "keystroke-" + target;
+        if (activeKeystroke.key === null) {
+            console.log('do nothing');
+        } else {
+            // if (!activeKeystroke.isReleased) {
+            //     console.log(document.getElementById(highlightedKey).classList.add('active'));
+            // } else {
+            //     console.log(document.getElementById(highlightedKey).classList.remove('active'));
+            // }
         }
     }
 
     useEffect(() => {
         document.addEventListener("keydown", handleKeyDown);
-    }, [])
+        document.addEventListener("keyup", handleKeyUp);
+        highlightActiveKey(activeKeystroke.key);
+    }, [activeKeystroke.key, activeKeystroke.isReleased])
+
       
 
     return (
         <Fragment>
             <footer className="keyboard-group-container">
                 <div className="keyboard-group">
-                    <div onKeyDown={handleKeyDown} id="keystrokeR" name="keystrokeR" className="keyboard-item">R</div>
-                    <div onKeyDown={handleKeyDown} id="keystrokeEscape" name="keystrokeEscape" className="keyboard-item">esc</div>
+                    <div onKeyDown={handleKeyDown} id="keystroke-R" name="keystroke-R" className={`keyboard-item`}>R</div>
+                    <div onKeyDown={handleKeyDown} id="keystroke-Escape" name="keystroke-Escape" className={`keyboard-item`}>esc</div>
                 </div>
                 <div className="keyboard-group">
-                    <div onKeyDown={handleKeyDown} id="keystrokeZ" name="keystrokeZ" className="keyboard-item">Z</div>
-                    <div onKeyDown={handleKeyDown} id="keystrokeX" name="keystrokeX" className="keyboard-item">X</div>
-                    <div onKeyDown={handleKeyDown} id="keystrokeC" name="keystrokeC" className="keyboard-item">C</div>
+                    <div onKeyDown={handleKeyDown} id="keystroke-Z" name="keystroke-Z" className={`keyboard-item`}>Z</div>
+                    <div onKeyDown={handleKeyDown} id="keystroke-X" name="keystroke-X" className={`keyboard-item`}>X</div>
+                    <div onKeyDown={handleKeyDown} id="keystroke-C" name="keystroke-C" className={`keyboard-item`}>C</div>
                 </div>
                 <div className="keyboard-group direction-keys">
                     <div>
-                        <div onKeyDown={handleKeyDown} id="keystrokeArrowUp" name="keystrokeArrowUp" className="keyboard-item">↑</div>
+                        <div onKeyDown={handleKeyDown} id="keystroke-ArrowUp" name="keystroke-ArrowUp" className={`keyboard-item`}>↑</div>
                     </div>
                     <div>
-                        <div onKeyDown={handleKeyDown} id="keystrokeArrowLeft" name="keystrokeArrowLeft" className="keyboard-item">←</div>
-                        <div onKeyDown={handleKeyDown} id="keystrokeArrowDown" name="keystrokeArrowDown" className="keyboard-item">↓</div>
-                        <div onKeyDown={handleKeyDown} id="keystrokeArrowRight" name="keystrokeArrowRight" className="keyboard-item">→</div>
+                        <div onKeyDown={handleKeyDown} id="keystroke-ArrowLeft" name="keystroke-ArrowLeft" className={`keyboard-item`}>←</div>
+                        <div onKeyDown={handleKeyDown} id="keystroke-ArrowDown" name="keystroke-ArrowDown" className={`keyboard-item`}>↓</div>
+                        <div onKeyDown={handleKeyDown} id="keystroke-ArrowRight" name="keystroke-ArrowRight" className={`keyboard-item`}>→</div>
                     </div>
                 </div>
             </footer>
--- client/src/contexts/AppSettingsContext.js
+++ client/src/contexts/AppSettingsContext.js
@@ -11,6 +11,109 @@ const AppSettingsContextProvider = ({ children }) => {
         production: 'http://0.0.0.0:5000',
     });
 
+    const [activeKeystroke, setActiveKeystroke] = useState({
+        key: '',
+        isReleased: true,
+    });
+
+    const handleKeyDown = (e) => {
+        switch(e.key) {
+            case "ArrowUp":
+                console.log("Pressed: ArrowUp!");
+                setActiveKeystroke({ key: 'ArrowUp', isReleased: false});
+                break;
+            case "ArrowDown":
+                console.log("Pressed: ArrowDown!");
+                setActiveKeystroke({ key: 'ArrowDown', isReleased: false});
+                break;
+            case "ArrowLeft":
+                console.log("Pressed: ArrowLeft!");
+                setActiveKeystroke({ key: 'ArrowLeft', isReleased: false});
+                break;
+            case "ArrowRight":
+                console.log("Pressed: ArrowRight!");
+                setActiveKeystroke({ key: 'ArrowRight', isReleased: false});
+                break;
+
+            case "Escape":
+                console.log("Pressed: Escape!");
+                setActiveKeystroke({ key: 'Escape', isReleased: false});
+                break;
+
+            case "z":
+                console.log("Pressed: z");
+                setActiveKeystroke({ key: 'z', isReleased: false});
+                break;
+
+            case "x":
+                console.log("Pressed: x");
+                setActiveKeystroke({ key: 'x', isReleased: false});
+                break;
+
+            case "c":
+                console.log("Pressed: c");
+                setActiveKeystroke({ key: 'c', isReleased: false});
+                break;
+
+            case "r":
+                console.log("Pressed: r");
+                setActiveKeystroke({ key: 'r', isReleased: false});
+                break;
+                
+            default:
+                break;
+        }
+    }
+
+    const handleKeyUp = (e) => {
+        switch(e.key) {
+            case "ArrowUp":
+                console.log("Released: ArrowUp!");
+                setActiveKeystroke({ key: 'ArrowUp', isReleased: true});
+                break;
+            case "ArrowDown":
+                console.log("Released: ArrowDown!");
+                setActiveKeystroke({ key: 'ArrowDown', isReleased: true});
+                break;
+            case "ArrowLeft":
+                console.log("Released: ArrowLeft!");
+                setActiveKeystroke({ key: 'ArrowLeft', isReleased: true});
+                break;
+            case "ArrowRight":
+                console.log("Released: ArrowRight!");
+                setActiveKeystroke({ key: 'ArrowRight', isReleased: true});
+                break;
+
+            case "Escape":
+                console.log("Released: Escape!");
+                setActiveKeystroke({ key: 'Escape', isReleased: true});
+                break;
+
+            case "z":
+                console.log("Released: z");
+                setActiveKeystroke({ key: 'z', isReleased: true});
+                break;
+
+            case "x":
+                console.log("Released: x");
+                setActiveKeystroke({ key: 'x', isReleased: true});
+                break;
+
+            case "c":
+                console.log("Released: c");
+                setActiveKeystroke({ key: 'c', isReleased: true});
+                break;
+
+            case "r":
+                console.log("Released: r");
+                setActiveKeystroke({ key: 'r', isReleased: true});
+                break;
+                
+            default:
+                break;
+        }
+    }
+
     const [ status, setStatus ] = useState([
         {
             message: "Idle, waiting for commands",
@@ -83,7 +186,7 @@ const AppSettingsContextProvider = ({ children }) => {
     }
 
     return (
-        <AppSettingsContext.Provider value={{ apiRoutes, blinkLed, testSocketConnection, status }}>
+        <AppSettingsContext.Provider value={{ apiRoutes, blinkLed, testSocketConnection, status, activeKeystroke, setActiveKeystroke, handleKeyDown, handleKeyUp }}>
             {children}
         </AppSettingsContext.Provider>
     )
