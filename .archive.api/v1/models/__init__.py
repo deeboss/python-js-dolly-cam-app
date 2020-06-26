19ee01d7a3b762c4fb4a2b33575455ebc29f5260
Don <hello@donbos.co>
2020-06-26 13:12:25 +0800

    removing v1 of backend api

--- .archive.api/v1/models/__init__.py
+++
@@ -1,6 +0,0 @@
-import os
-from models.motor import Motor
-from models.servo import Servo
-from models.easingFunctions import easeFunctions
-from models.buzzer import Buzzer
-# from models.camera import Camera
--- .archive.api/v1/views/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-app_template_views = Blueprint('app_template_views', __name__, template_folder='templates')
-from views.index import *  # noqa
-from views.gpio import *  # noqa
-from views.motor import *  # noqa
-from views.servo import *  # noqa
-# from views.camera import *  # noqa
