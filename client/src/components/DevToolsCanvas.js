bd009f7212a03795b8a77628884adb0ce6b04eb6
Don <hello@donbos.co>
2020-06-28 02:14:56 +0800

    backbutton and header layout

--- client/src/components/DevToolsCanvas.js
+++ client/src/components/DevToolsCanvas.js
@@ -1,5 +1,9 @@
 import React, { Fragment, useState, useContext } from 'react';
+import {Link} from "react-router-dom";
+
 import '../assets/css/styles.scss';
+import BackArrow from "../assets/img/icons/arrow-left.svg";
+
 import { AppSettingsContext } from '../contexts/AppSettingsContext';
 import { VehicleContext } from '../contexts/VehicleContext';
 import Joystick from './Joystick';
@@ -16,6 +20,11 @@ const DevToolsCanvas = () => {
             <StatusBar/>
             <div className="wrapper">
                 <div className="app-container">
+                    <header>
+                        <div><Link to="/"><img className="icon-s" alt="" src={BackArrow}/></Link></div>
+                        <div><h3>Dev Tools</h3></div>
+                        <div></div>
+                    </header>
                     <div className="module"> 
                         <WaypointsChart/>
                         <Joystick />
