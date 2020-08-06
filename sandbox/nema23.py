d45b1fd8d7420843afabb931a0526a5b4eb937b4
deeboss <hello@donbos.co>
2020-08-06 16:28:49 +0800

    nema 23 tests

--- sandbox/nema23.py
+++ sandbox/nema23.py
@@ -6,7 +6,7 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11,GPIO.OUT)
 
-for i in range(0,400):
+for i in range(0,1200):
     GPIO.output(11,GPIO.HIGH)
-    time.sleep(0.01)
+    time.sleep(0.0013)
     GPIO.output(11,GPIO.LOW)
